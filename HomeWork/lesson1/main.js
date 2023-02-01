84a18d92c417956798bf3f12d7b2002acc7dace5
Veronika-Uppie <veronikauppie@gmail.com>
2023-02-01 13:16:45 +0300

    HomeWork

---
+++ HomeWork/lesson1/main.js
@@ -0,0 +1,55 @@
+
+let hello = 'hello';
+console.log(hello);
+
+let owu = 'owu';
+console.log(owu);
+
+let com = 'com';
+console.log(com);
+
+let ua = 'ua';
+console.log(ua);
+
+let q = 1;
+console.log(q);
+
+let w = 10;
+console.log(w);
+
+let r = -999;
+console.log(r);
+
+let d = 123;
+console.log(d);
+
+const PI = 3.14;
+console.log(PI);
+
+let e = 2.7;
+console.log(e);
+
+let f = 16;
+console.log(f);
+
+let g = true;
+console.log(g);
+
+let h = false;
+console.log(h);
+
+let firstName = 'Veronika';
+let middleName = 'Uppie';
+let lastName = 'Oleksandrivna';
+
+let person =`${firstName} ${middleName} ${lastName}`;
+
+console.log(person)
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
