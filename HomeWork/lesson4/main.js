16c866b3009d7f3aea2f930f7b094798da1f6416
Veronika-Uppie <veronikauppie@gmail.com>
2023-02-12 17:58:42 +0200

    hw

--- HomeWork/lesson4/main.js
+++ HomeWork/lesson4/main.js
@@ -38,12 +38,6 @@ function display_elements(array) {
 let users = [
     {name: 'vasya', age: 31, status: false},
     {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'vera', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
     {name: 'masha', age: 30, status: true},
     {name: 'olya', age: 31, status: false},
     {name: 'max', age: 31, status: true}
@@ -63,7 +57,8 @@ function creates_paragraph(p_text){
     );
 }
 
-creates_paragraph('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime perspiciatis quae, recusandae sed suscipit tenetur veniam! Impedit magnam necessitatibus numquam quam ullam. Deleniti et fugiat iusto laboriosam nam neque perspiciatis?');
+let p_text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime perspiciatis quae, recusandae sed suscipit tenetur veniam! Impedit magnam necessitatibus numquam quam ullam. Deleniti et fugiat iusto laboriosam nam neque perspiciatis?';
+creates_paragraph(p_text);
 
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
@@ -98,15 +93,15 @@ function count_li(li,number) {
   document.write(`</ul>`);
     return count_li;
 }
-
-count_li('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis dignissimos eligendi ex facere inventore itaque molestiae natus, necessitatibus neque nisi, non odio omnis quas quis, quos sint veniam voluptatem.', 10);
+let list = 'lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis dignissimos eligendi ex facere inventore itaque molestiae natus, necessitatibus neque nisi, non odio omnis quas quis, quos sint veniam voluptatem.';
+count_li(list, 10);
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 
 document.write(`Список, побудованний по массиву`);
 
-let elements = [4568, 'Hello', true, false];
-function create_list_by_arr(arr) {
+let arr = [4568, 'Hello', true, false];
+function create_list_by_arr(elements) {
         document.write(`<ul>`);
     for (let element of elements) {
         document.write(`<li>${element}</li>`)
@@ -115,11 +110,33 @@ function create_list_by_arr(arr) {
     return create_list_by_arr;
 }
 
-create_list_by_arr(elements);
-
-
+create_list_by_arr(arr);
 
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+let persons = [
+    {id: 1, name: 'vasya', age: 31},
+    {id: 2, name: 'petya', age: 30},
+    {id: 3, name: 'kolya', age: 29},
+    {id: 4, name: 'olya', age: 28},
+    {id: 5, name: 'max', age: 31}
+];
+
+function display_arr_el(arr) {
+    for (const item of arr) {
+        document.write(`<div>`);
+            for (let element in item) {
+                document.write(`<p>${element}</p>`)
+            }
+        document.write(`</div>`);
+    }
+    return display_arr_el;
+}
+
+display_arr_el(persons);
+
+
+
 // - створити функцію яка повертає найменьше число з масиву
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
