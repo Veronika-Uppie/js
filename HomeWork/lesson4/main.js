dda45d43077e4461f0b7d7f08d3fee9992b83b4c
Veronika-Uppie <veronikauppie@gmail.com>
2023-02-13 17:45:22 +0200

    hw

--- HomeWork/lesson4/main.js
+++ HomeWork/lesson4/main.js
@@ -186,7 +186,7 @@ console.log('функція міняє місцями занчення у від
 
 let arr1 = [11,22,33,44];
 function swap(arr,index1,index2) {
-    for(let i = 0; i < arr.length; i++)
+    for (let i = 0; i < arr.length; i++) {
         if (i === index1) {
             let index3 = arr[index1]
             arr[index1] = arr[index2]
@@ -197,7 +197,10 @@ function swap(arr,index1,index2) {
         if (index1 >= arr.length || index2 >= arr.length) {
             console.log('Error')
         }
+
     }
+
+}
 
 swap(arr1, 3, 0);
 swap(arr1, 4, 0);
@@ -206,12 +209,19 @@ swap(arr1, 4, 0);
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
 
-// console.log('функція обміну валюти');
-//
-// function exchange(sumUAH,currencyValues,exchangeCurrency){
-//     for (let item of currencyValues){
-//
-//     }
-// }
-//
-// exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD');
+console.log('функція обміну валюти');
+
+
+
+function exchange(sumUAH,currencyValues,exchangeCurrency){
+    for (let item of currencyValues){
+        if (item.currency === exchangeCurrency) {
+            let result = sumUAH / item.value
+            console.log(result);
+            return result;
+        }
+    }
+}
+
+exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD');
+
