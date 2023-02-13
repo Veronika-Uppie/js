76617e787abc62cc3ddbd45702ccfa6f2138f2c7
Veronika-Uppie <veronikauppie@gmail.com>
2023-02-13 16:17:49 +0200

    hw

--- HomeWork/lesson4/main.js
+++ HomeWork/lesson4/main.js
@@ -182,14 +182,36 @@ sum(arr_of_num);
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відповідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
 
-// console.log('функція місцями заняення у відповідних індексах');
-//
-// let arr1 = [11,22,33,44];
-// function swap(arr,index1,index2) {
-//
-// }
+console.log('функція міняє місцями занчення у відповідних індексах');
 
+let arr1 = [11,22,33,44];
+function swap(arr,index1,index2) {
+    for(let i = 0; i < arr.length; i++)
+        if (i === index1) {
+            let index3 = arr[index1]
+            arr[index1] = arr[index2]
+            arr[index2] = index3
+            console.log(arr);
+            return swap;
+        }
+        if (index1 >= arr.length || index2 >= arr.length) {
+            console.log('Error')
+        }
+    }
+
+swap(arr1, 3, 0);
+swap(arr1, 4, 0);
 
 
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+
+// console.log('функція обміну валюти');
+//
+// function exchange(sumUAH,currencyValues,exchangeCurrency){
+//     for (let item of currencyValues){
+//
+//     }
+// }
+//
+// exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD');
