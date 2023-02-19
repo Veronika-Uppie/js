72bdbb4e7c1a0766b9d527df60ca5ed3e10eeedf
Veronika-Uppie <veronikauppie@gmail.com>
2023-02-19 19:47:13 +0200

    hw

--- HomeWork/lesson6/main.js
+++ HomeWork/lesson6/main.js
@@ -53,7 +53,7 @@ stringToarray(str_1);
 
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
 
-let num_arr = [10,8,-7,55,987,-1011,0,1050,0];
+// let num_arr = [10,8,-7,55,987,-1011,0,1050,0];
 
 
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
@@ -82,16 +82,20 @@ let num_arr = [10,8,-7,55,987,-1011,0,1050,0];
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
 // -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
 
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
 
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 
+// let filter1
+let filter2 = coursesAndDurationArray.filter(user => user.monthDuration > 5);
+console.log(filter2);
+// let filter3
 
 
 // =========================
