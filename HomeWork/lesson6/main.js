82dc91a236e93e6b3ae86d0187f894e928250a81
Veronika-Uppie <veronikauppie@gmail.com>
2023-02-19 19:29:32 +0200

    hw

--- HomeWork/lesson6/main.js
+++ HomeWork/lesson6/main.js
@@ -10,20 +10,65 @@ console.log (str3.length);
 
 // - Перевести до великого регістру наступні стрінгові значення
 // 'hello world', 'lorem ipsum', 'javascript is cool'
+
+console.log(str1.toUpperCase());
+console.log(str2.toUpperCase());
+console.log(str3.toUpperCase());
+
+
 // - Перевести до нижнього регістру настипні стрінгові значення
 // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+let str4 = 'HELLO WORLD';
+let str5 = 'LOREM IPSUM';
+let str6 = 'JAVASCRIPT IS COOL';
+
+console.log(str4.toLowerCase());
+console.log(str5.toLowerCase());
+console.log(str6.toLowerCase());
+
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
-//
+
+// !!!!!!!
+
+
+
+
+let str = ' dirty string   '
+console.log (str.length);
+let p1 = str.replaceAll(' ', '')
+console.log(p1);
+console.log(p1.length);
+
+
 // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
 //     let str = 'Ревуть воли як ясла повні';
 // let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
-//
+
+
+let str_1 = 'Ревуть воли як ясла повні';
+let stringToarray = (str) => console.log(str.split(' '));
+
+stringToarray(str_1);
+
+
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+
+let num_arr = [10,8,-7,55,987,-1011,0,1050,0];
+
+
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
 //     let nums = [11,21,3];
 // sortNums(nums,'ascending') // [3,11,21]
 // sortNums(nums,'descending') // [21,11,3]
+
+// let sortNums = (direction) =>{
 //
+// }
+//
+// sortNums(nums,'ascending');
+// sortNums(nums,'descending');
+
 // ==========================
 // - є масив
 // let coursesAndDurationArray = [
@@ -37,6 +82,19 @@ console.log (str3.length);
 // -- відсортувати його за спаданням за monthDuration
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
 // -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+
+
+
+
 // =========================
 //     описати колоду карт (від 6 до туза без джокерів)
 // - знайти піковий туз
