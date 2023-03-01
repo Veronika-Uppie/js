5d1cceef0950bffafd763c0c09a869ed3af4d31d
Veronika-Uppie <veronikauppie@gmail.com>
2023-03-01 18:55:11 +0200

    дз

--- HomeWork/lesson9/main.js
+++ HomeWork/lesson9/main.js
@@ -286,17 +286,7 @@ for (let element of coursesArray) {
 
 
 //     - Створити довільний елемент з id = text та створити кнопку.Використовуючи JavaScript, зробіть так, щоб при натисканні на кнопку зникав елемент з id="text".
-//
-// <div id="elem1">
-//     <p>
-//     Этот текст нужно удалить
-// <button id="text" onclick="deletedItem()">Удалить</button>
-// </p>
-// </div>
 
-// function deletedItem(idItem) {
-//     document.getElementById(idItem).remove();
-// }
 
 let element = document.createElement('div');
 let button = document.createElement('button');
@@ -307,8 +297,6 @@ button.style.width = '150px';
 button.style.height = '50px';
 p.id = 'text';
 button.id = 'button'
-
-
 
 
 element.append(button, p );
