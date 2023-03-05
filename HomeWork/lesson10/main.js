f1c9225fc8350f36751d238941245fad4ba8c319
Veronika-Uppie <veronikauppie@gmail.com>
2023-03-05 13:29:22 +0200

    дз

--- HomeWork/lesson10/main.js
+++ HomeWork/lesson10/main.js
@@ -14,10 +14,50 @@ userForm.onsubmit = function (eo) {
 // ==========================
 // є сторінка, на якій є блок, я кому знаходиьтся цифра. написати код, який при кожному перезавантажені сторінки буде додавати до неї +1
 
+let number = document.getElementById('number');
+
+
+window.onload = function () {
+
+    localStorage.setItem('keyNumber',number.textContent);
+
+    let keyNumberValue = JSON.parse(localStorage.getItem('keyNumber'));
+    console.log(keyNumberValue);
+    console.log(typeof keyNumberValue);
+
+
+    let neuNumber = keyNumberValue + 1;
+    console.log(neuNumber);
+
+    // newNumber = JSON.stringify(keyNumberNumber + 1);
+    // keyNumber = newNumber;
+
+}
+
+// let number = document.getElementById('number');
+// console.log(number.value);
+// number.onclick = function (){
+//     localStorage.setItem('keyNumber',JSON.stringify(0));
+//
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 // ==========================
 // Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховще, в масив sessions зберігається інформація про дату та час відвідування сторінки. Є ще сторінка sessions.html (назва довільна), при відвідуванні якої потрібно відмалювати всю інформацію про відвідування сторінки index.html. Інфу НЕ виводити в консоль, а побудувати дом структуру під кожну сессію
+
+
+
 // =========================
 //     зробити масив на 100 об'єктів та дві кнопки prev next
 // при завантажені сторінки з'являються перші 10 об'єктів.
