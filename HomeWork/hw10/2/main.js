79d664034ff9afd195326d3223bddd6b4f16fd53
Veronika-Uppie <veronikauppie@gmail.com>
2023-03-06 20:07:13 +0200

    дз

--- HomeWork/hw10/2/main.js
+++ HomeWork/hw10/2/main.js
@@ -3,9 +3,10 @@
 // є сторінка, на якій є блок, я кому знаходиьтся цифра. написати код, який при кожному перезавантажені сторінки буде додавати до неї +1
 
 
+
 let num = localStorage.getItem('number') || 0;
 let numberText = document.getElementById('number');
 
-let newNumber = (+num +1).toString();
+let newNumber = JSON.stringify((JSON.parse(num) +1));
 numberText.innerText = newNumber;
 localStorage.setItem('number', newNumber);
